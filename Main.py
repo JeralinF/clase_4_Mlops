d2266c85ddb76fbf5e5ba402ada1949fadbc53a9
Jeralin <jeralinflores4@gmail.com>
2024-02-22 16:46:11 -0600

    suma y resta

--- Main.py
+++ Main.py
@@ -4,3 +4,13 @@ def suma (num_1: int, num_2: int) -> int:
 
 if __name__ == '__main__':
     print(suma(num_1=2, num_2=1))
+
+
+def resta (num_1: int, num_2: int) -> int:
+    return num_1 -  num_2
+
+
+if __name__ == '__main__':
+    print(resta(num_1=2, num_2=1))
+    print(suma(num_1=2, num_2=1))
+
