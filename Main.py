e1fb6f4e66aacfd62994911cfae380cd7d203c03
Jeralin <jeralinflores4@gmail.com>
2024-02-27 18:13:19 -0600

    agrege en riq al repositorio

--- Main.py
+++ Main.py
@@ -2,15 +2,21 @@ def suma (num_1: int, num_2: int) -> int:
     return num_1 + num_2
 
 
-if __name__ == '__main__':
-    print(suma(num_1=2, num_2=1))
-
-
 def resta (num_1: int, num_2: int) -> int:
     return num_1 -  num_2
+
+
+
+
+def gato(animal: str):
+    if animal == 'cat':
+        return True
+    else:
+        return False
+
 
 
 if __name__ == '__main__':
     print(resta(num_1=2, num_2=1))
     print(suma(num_1=2, num_2=1))
-
+    print(gato(animal='cat'))
